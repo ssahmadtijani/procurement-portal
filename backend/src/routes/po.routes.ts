d1f373de1450f9b38cd83dff8a936240447cf504
ultradev <ssahmadtijani@gmail.com>
2026-05-26 20:45:33 +0100

    fix: allow ORG_ADMIN and CORPORATE_OFFICE to send/update POs

--- backend/src/routes/po.routes.ts
+++ backend/src/routes/po.routes.ts
@@ -16,8 +16,8 @@ router.use(authenticate);
 
 router.get('/', listPurchaseOrders);
 router.get('/:id', getPurchaseOrderById);
-router.put('/:id', authorize('PROCUREMENT_OFFICER'), updatePODetails);
-router.post('/:id/send', authorize('PROCUREMENT_OFFICER'), sendPurchaseOrder);
+router.put('/:id', authorize('PROCUREMENT_OFFICER', 'ORG_ADMIN', 'CORPORATE_OFFICE'), updatePODetails);
+router.post('/:id/send', authorize('PROCUREMENT_OFFICER', 'ORG_ADMIN', 'CORPORATE_OFFICE'), sendPurchaseOrder);
 router.post('/:id/acknowledge', authorize('SUPPLIER', 'ORG_ADMIN'), acknowledgePO);
 router.post('/:id/complete', authorize('PROCUREMENT_OFFICER', 'CORPORATE_OFFICE'), completePO);
 
